c6b3315e4c09c5c3022e1922d985b71aaecb0d72
Michael Simon <34105770+heckfeld@users.noreply.github.com>
2024-12-08 11:26:11 +0100

    Update axp192.h

--- components/axp192/axp192.h
+++ components/axp192/axp192.h
@@ -3,7 +3,7 @@
 
 // #include "esphome/core/component.h"
 // #include "esphome/components/sensor/sensor.h"
-// #include "esphome/components/i2c/i2c.h"
+#include "esphome/components/i2c/i2c.h"
 
 namespace esphome {
 namespace axp192 {
